905a3aefef683a352f96ed2cc7487c7b9f9dd64a
MeIchthys <10717998+meichthys@users.noreply.github.com>
2021-09-03 10:23:57 -0400

    Fix Sub-sub page export
    Previously there was an issue with sub-sub pages not properly being exported to a sub-directory. 
    This fixes that and adds a small terminal output change: indent sections, pages, attachments for easier review when script finishes.

--- onenote-powershell-html-export.ps1
+++ onenote-powershell-html-export.ps1
@@ -6,82 +6,98 @@
 
 # Get export folder
 Function Get-Folder($initialDirectory) {
-	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
-	$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-	$foldername.Description = "Select an export folder"
-	$foldername.rootfolder = "MyComputer"
-	if($foldername.ShowDialog() -eq "OK")
-	{
-		$folder += $foldername.SelectedPath
-	}
-	return $folder
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select an export folder"
+    $foldername.rootfolder = "MyComputer"
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
 }
 
 # Spider and find each page, create directory for each group
 Function Spider-OneNote-Notebook {
-	param( $onenote, $node, $path )
-	foreach($child in $node.ChildNodes) {
-		if ($child.pageLevel -eq 1) {
-			$parent = $child.name
-		} elseif ($child.pageLevel -eq 2) {
-			$grandparent = $parent
-		}
-		if ($child.HasChildNodes) {
-			if ($child.isRecycleBin -ne 'true') {
-				$folder = Join-Path -Path $path -ChildPath $child.name
-				New-Item -Path $folder -ItemType directory | Out-Null
-				Write-Host "Section: $($folder)"
-				Spider-OneNote-Notebook -onenote $onenote -node $child -path $folder
-			}
+    param( $onenote, $node, $path )
+    $previouslevel = 0
+    $previousname = ""
+    $grandparent = ""
+    $parent = ""
+    foreach($child in $node.ChildNodes) {
+        $child.name = ReplaceIllegal -text $child.name
+        $levelchange = $child.pageLevel - $previouslevel
+        if (-not $child.HasChildNodes) {
+            if ($levelchange -eq 1) {
+                if ($previouslevel -ne 0) {
+                    $grandparent = $parent
+                    $parent = $previousname
+                }
+                $filepath = Join-Path -path $(join-path -path $path -ChildPath $grandparent) -ChildPath $parent
+                New-Item -Path $filepath -ItemType directory -ErrorAction Ignore | Out-Null
+                Export-OneNote-Page -onenote $onenote -node $child -path $filepath
 
-		} else {
+            } elseif ($levelchange -eq -1) {
+                $filepath = Join-Path -path $path -ChildPath $grandparent
+                New-Item -Path $filepath -ItemType directory -ErrorAction Ignore | Out-Null
+                Export-OneNote-Page -onenote $onenote -node $child -path $filepath
+                $parent = $grandparent
+                $grandparent = ""
+            } elseif ($levelchange -eq -2) {
+                Export-OneNote-Page -onenote $onenote -node $child -path $path
+                $parent = ""
+                $grandparent = ""
+            } elseif ($levelchange -eq 0 -and $parent -eq "") {
+                Export-OneNote-Page -onenote $onenote -node $child -path $path
+            } else {
+                $grandparentpath = Join-Path -path $path -ChildPath $grandparent
+                $filepath = Join-Path -path $grandparentpath -ChildPath $parent
+                New-Item -Path $filepath -ItemType directory -ErrorAction Ignore | Out-Null
+                Export-OneNote-Page -onenote $onenote -node $child -path $filepath
+            }
+        } else {
+            $folder = Join-Path -Path $path -ChildPath $child.name
+            New-Item -Path $folder -ItemType directory -ErrorAction Ignore | Out-Null
+            Write-Host "  Section: $($folder)"
+            Spider-OneNote-Notebook -onenote $onenote -node $child -path $folder
+        }
+        # Store page level
+        $previouslevel = $child.pageLevel
+        $previousname = $child.name
+    }
 
-			if ($child.pageLevel -eq 2) {
-				$newpath = Join-Path -path $path -ChildPath $grandparent
-				New-Item -Path $newpath -ItemType directory | Out-Null
-				Export-OneNote-Page -onenote $onenote -node $child -path $newpath
-			} elseif ($child.pageLevel -eq 3) {
-				$newpath = Join-Path -path $path -ChildPath $grandparent -ChildPath $parent
-				New-Item -Path $newpath -ItemType directory | Out-Null
-				Export-OneNote-Page -onenote $onenote -node $child -path $newpath
-			} else {
-				Export-OneNote-Page -onenote $onenote -node $child -path $path
-			}
-
-		}
-	}
 }
 
 # Export page
 Function Export-OneNote-Page {
-	param( $onenote, $node, $path )
-	# Replace invalid file characters
-	$name = ReplaceIllegal -text $node.name
-	$file = $(Join-Path -Path $path -ChildPath "$($name).htm")
-	Write-Host "Page: $($file)"
-	# Export
-	$onenote.Publish($node.ID, $file, 7, "")
-	Export-OneNote-Attachments -onenote $onenote -node $node -path $path
+    param( $onenote, $node, $path )
+    # Replace invalid file characters
+    $name = ReplaceIllegal -text $node.name
+    $file = $(Join-Path -Path $path -ChildPath "$($name).htm")
+    Write-Host "    Page: $($file)"
+    # Export
+    $onenote.Publish($node.ID, $file, 7, "")
+    Export-OneNote-Attachments -onenote $onenote -node $node -path $path
 }
 
 # Copy embedded attachments
 Function Export-OneNote-Attachments {
-	param ( $onenote, $node, $path )
-	$xml = ''
-	$schema = @{one=”http://schemas.microsoft.com/office/onenote/2013/onenote”}
-	$onenote.GetPageContent($node.ID, [ref]$xml)
-	$xml | Select-Xml -XPath "//one:Page/one:Outline/one:OEChildren/one:OE/one:InsertedFile" -Namespace $schema | foreach {
-		$file = Join-Path -Path $path -ChildPath $_.Node.preferredName
-		Write-Host "Attachment: $($file)"
-		Copy-Item $_.Node.pathCache -Destination $file
-	}
+    param ( $onenote, $node, $path )
+    $xml = ''
+    $schema = @{one=”http://schemas.microsoft.com/office/onenote/2013/onenote”}
+    $onenote.GetPageContent($node.ID, [ref]$xml)
+    $xml | Select-Xml -XPath "//one:Page/one:Outline/one:OEChildren/one:OE/one:InsertedFile" -Namespace $schema | foreach {
+        $file = Join-Path -Path $path -ChildPath $_.Node.preferredName
+        Write-Host "      Attachment: $($file)"
+        Copy-Item $_.Node.pathCache -Destination $file
+    }
 }
 
 Function ReplaceIllegal {
-	param ( $text )
-	$illegal = [string]::join('',([System.IO.Path]::GetInvalidFileNameChars())) -replace '\\','\\'
-	$replaced = $text -replace "[$illegal]",'_'
-	return $replaced
+    param ( $text )
+    $illegal = [string]::join('',([System.IO.Path]::GetInvalidFileNameChars())) -replace '\\','\\'
+    $replaced = $text -replace "[$illegal]",'_'
+    return $replaced
 }
 
 # Get export folder
@@ -94,9 +110,9 @@ $OneNote.GetHierarchy("", [Microsoft.Office.InterOp.OneNote.HierarchyScope]::hsP
 
 # Loop over each notebook
 foreach ($notebook in $Hierarchy.Notebooks.Notebook ) {
-	$name = ReplaceIllegal -text $notebook.name
-	$nf = Join-Path -Path $folder -ChildPath $name
-	Write-Host "Notebook: $($nf)"
-	New-Item -Path $nf -ItemType directory | Out-Null
-	Spider-OneNote-Notebook -onenote $OneNote -node $notebook -path $nf
+    $name = ReplaceIllegal -text $notebook.name
+    $nf = Join-Path -Path $folder -ChildPath $name
+    Write-Host "Notebook: $($nf)"
+    New-Item -Path $nf -ItemType directory | Out-Null
+    Spider-OneNote-Notebook -onenote $OneNote -node $notebook -path $nf
 }
